61ccc21936f5bf934b050a6cecf26a7a67c09427
Shrutibeladia <shrutibeladi21@gmail.com>
2023-02-20 10:19:34 +0530

    third

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './mycomponents/Footer';
 function App() {
   return (
        <>
-       <Header/>
+       <Header title="Shruti's Todos List"/>
        <Todos />
        <Footer />
 
