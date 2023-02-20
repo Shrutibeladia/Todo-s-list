2e5177ae187b5a7f0358cf7775dcd5ca24519a24
Shrutibeladia <shrutibeladi21@gmail.com>
2023-02-20 10:16:49 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './mycomponents/Header';
+import Todos from './mycomponents/Todos';
+import Footer from './mycomponents/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <>
+       <Header title="Shruti's Todos List"/>
+       <Todos />
+       <Footer />
+
+       </>
   );
 }
 
